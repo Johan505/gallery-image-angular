18b1e93b7d53605f17f8c1be6d38f49764756010
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-05-25 13:40:31 -0500

    chore(config): instaling scss and restrure files.

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.css']
+  styleUrls: ['./title.component.scss']
 })
 export class TitleComponent {
 
-  title:String[]= ['Johan Avendaño Y Diego', 'Mi viaje por el mundo']
+  title:String= 'Johan Avendaño Y Diego';
+  subtitle:String = 'Mi viaje por el mundo';
 
 }
